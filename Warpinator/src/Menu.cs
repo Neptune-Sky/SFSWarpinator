4917e663f433492e4858f5138afd24829dcf3325
Neptune-Sky <toastybreadbread@gmail.com>
2023-02-06 06:05:16 -0600

    Moved teleport code to separate method

--- Warpinator/src/Menu.cs
+++ Warpinator/src/Menu.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using HarmonyLib;
 using SFS.UI;
 using static SFS.UI.MenuGenerator;
 using static SFS.UI.ButtonBuilder;
@@ -9,7 +8,6 @@ using SFS;
 using SFS.Audio;
 using SFS.World;
 using SFS.World.Maps;
-using UnityEngine;
 
 namespace Warpinator
 {
@@ -40,17 +38,7 @@ namespace Warpinator
 
                 ButtonBuilder button = CreateButton(null, () => planet.DisplayName, () =>
                 {
-                    MsgDrawer.main.Log("Teleporting to " + planet.DisplayName);
-                    var indexOf = GameManager.main.rockets.IndexOf(PlayerController.main.player.Value as Rocket);
-                    PlayerController.main.player.Value = null;
-                    Double2 parameters = GetOrbitParameters(planet);
-                    Debug.Log(parameters);
-                    Location nullLocation = new Location(0, planet, new Double2(0, 0), new Double2(0, 0));
-                    Location location = new Location(0, planet, new Double2(0, planet.Radius + parameters.x), new Double2(parameters.y, 0));
-                    GameManager.main.rockets[indexOf].physics.SetLocationAndState(nullLocation, false);
-                    GameManager.main.rockets[indexOf].physics.SetLocationAndState(location, false);
-                    PlayerController.main.player.Value = GameManager.main.rockets[indexOf];
-                    Map.view.ToggleFocus(GameManager.main.rockets[indexOf].mapPlayer);
+                    MoveRocket.Teleport(planet);
                 }, SFS.Input.CloseMode.Current);
 
                 horizontalElements.Add(button);
@@ -77,17 +65,6 @@ namespace Warpinator
             if (planets2 != planets) Create();
 
             OpenMenu(CancelButton.Cancel, SFS.Input.CloseMode.Current, menuElements.ToArray());
-        }
-
-        static Double2 GetOrbitParameters(Planet planet)
-        {
-            double distance = planet.HasAtmospherePhysics
-                ? planet.AtmosphereHeightPhysics * 1.1
-                : planet.TimewarpRadius_Descend * 1.1 - planet.Radius;
-
-            double velocity = Math.Sqrt(planet.mass / (distance + planet.Radius));
-
-            return new Double2(distance, velocity);
         }
     }
 }
--- Warpinator/src/MoveRocket.cs
+++ Warpinator/src/MoveRocket.cs
@@ -1,9 +1,38 @@
-﻿namespace Warpinator
+﻿using System;
+using SFS.UI;
+using SFS.World;
+using SFS.World.Maps;
+using SFS.WorldBase;
+
+namespace Warpinator
 {
     public class MoveRocket
     {
-        public MoveRocket()
+        public static void Teleport(Planet planet)
         {
+            MsgDrawer.main.Log("Teleporting to " + planet.DisplayName);
+            var indexOf = GameManager.main.rockets.IndexOf(PlayerController.main.player.Value as Rocket);
+            PlayerController.main.player.Value = null;
+            Double2 parameters = GetOrbitParameters(planet);
+            Location nullLocation = new Location(0, planet, new Double2(0, 0), new Double2(0, 0));
+            Location location = new Location(0, planet, new Double2(0, planet.Radius + parameters.x), new Double2(parameters.y, 0));
+                    
+            GameManager.main.rockets[indexOf].physics.SetLocationAndState(nullLocation, false);
+                    
+            GameManager.main.rockets[indexOf].physics.SetLocationAndState(location, false);
+            PlayerController.main.player.Value = GameManager.main.rockets[indexOf];
+            Map.view.ToggleFocus(GameManager.main.rockets[indexOf].mapPlayer);
+        }
+        
+        static Double2 GetOrbitParameters(Planet planet)
+        {
+            double distance = planet.HasAtmospherePhysics
+                ? planet.AtmosphereHeightPhysics * 1.1
+                : planet.TimewarpRadius_Descend * 1.1 - planet.Radius;
+
+            double velocity = Math.Sqrt(planet.mass / (distance + planet.Radius));
+
+            return new Double2(distance, velocity);
         }
     }
 }
