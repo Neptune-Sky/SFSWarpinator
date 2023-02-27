f09b3487796fde6d089da0783e655c435e941a78
Neptune-Sky <toastybreadbread@gmail.com>
2023-02-26 22:00:52 -0600

    split planet button creation into new method, search button and handler class

--- Warpinator/src/PlanetSelectMenu.cs
+++ Warpinator/src/PlanetSelectMenu.cs
@@ -8,6 +8,7 @@ using SFS.Audio;
 using SFS.UI.ModGUI;
 using SFS.World;
 using UnityEngine;
+using Button = SFS.UI.ModGUI.Button;
 using Type = SFS.UI.ModGUI.Type;
 
 namespace Warpinator
@@ -15,15 +16,17 @@ namespace Warpinator
     public static class PlanetSelectMenu
     {
         static List<MenuElement> menuElements = new();
-        static List<Planet> planets = new();
-
-        static void Create()
+        public static List<Planet> planets = new();
+        const float windowScale = 0.75f;
+        const float buttonTextScale = 0.875f;
+        
+        static void CreateDefault()
         {
             menuElements.Clear();
             planets.Clear();
             planets.AddRange(Base.planetLoader.planets.Values);
-            int columns = Math.Clamp((int)Math.Ceiling((double)(planets.Count / 12)), 2, 4);
-            int rows = Math.Clamp((int)Math.Ceiling((double)planets.Count / columns), 1, 13);
+            int columns = Mathf.Clamp((int)Math.Ceiling((double)(planets.Count / 13)), 2, 4);
+            int rows = Mathf.Clamp((int)Math.Ceiling((double)planets.Count / columns), 1, 13);
             
             var output = new MenuElement(delegate(GameObject root)
             {
@@ -33,14 +36,20 @@ namespace Warpinator
                 
                 var scroll = Builder.CreateWindow(rectTransform, Builder.GetRandomID(), 275 * columns, 50 + 58 * rows, 0, 0, false, false, 1, "Warpinator");
 
-                float scale = 0.75f;
+                scroll.Position = new Vector2(0, scroll.Size.y * windowScale / 2);
 
-                scroll.Position = new Vector2(0, scroll.Size.y * scale / 2);
+                var searchButton = Builder.CreateButton(scroll.gameObject.transform, 120, 42, 0, 0, SearchHandler.OpenMenu, "Search...");
+                searchButton.gameObject.GetComponentInChildren<TextAdapter>().gameObject.transform.localScale =
+                    new Vector3(buttonTextScale, buttonTextScale);
+                searchButton.gameObject.transform.localPosition = new Vector3(-(scroll.Size.x / 2) + searchButton.Size.x / 2 + 15, -(searchButton.Size.y / 2) - 5);
+                
                 var layout = scroll.CreateLayoutGroup(Type.Vertical);
                 layout.spacing = 7;
                 layout.childAlignment = TextAnchor.MiddleCenter;
                 scroll.EnableScrolling(Type.Vertical);
 
+                CreatePlanetButtons(planets, scroll);
+                /*
                 Container horizontal = Builder.CreateContainer(scroll);
                 horizontal.CreateLayoutGroup(Type.Horizontal);
                 for (int i = 0; i < planets.Count; i++)
@@ -56,13 +65,39 @@ namespace Warpinator
                     {
                         PlanetTeleportMenu.Open(planet);
                     }, planet.name);
+                    button.gameObject.GetComponentInChildren<TextAdapter>().gameObject.transform.localScale =
+                        new Vector3(buttonTextScale, buttonTextScale);
                 }
-
-                scroll.gameObject.transform.localScale = new Vector3(scale, scale);
+                */
+                scroll.gameObject.transform.localScale = new Vector3(windowScale, windowScale);
                 containerObject.transform.SetParent(root.transform);
             });
             
             menuElements.Add(output);
+        }
+
+        public static void CreatePlanetButtons(List<Planet> planetList, Transform parent)
+        {
+            int columns = Mathf.Clamp((int)Math.Ceiling((double)(planetList.Count / 13)), 2, 4);
+            
+            Container horizontal = Builder.CreateContainer(parent);
+            horizontal.CreateLayoutGroup(Type.Horizontal);
+            for (int i = 0; i < planetList.Count; i++)
+            {
+                Planet planet = planetList[i];
+
+                if (i % columns == 0 && i != 0)
+                {
+                    horizontal = Builder.CreateContainer(parent);
+                    horizontal.CreateLayoutGroup(Type.Horizontal);
+                }
+                var button = Builder.CreateButton(horizontal, 250, 50, 0, 0, () =>
+                {
+                    PlanetTeleportMenu.Open(planet);
+                }, planet.name);
+                button.gameObject.GetComponentInChildren<TextAdapter>().gameObject.transform.localScale =
+                    new Vector3(buttonTextScale, buttonTextScale);
+            }
         }
 
         public static void Open()
@@ -76,7 +111,7 @@ namespace Warpinator
             List<Planet> planets2 = new List<Planet>();
             planets2.AddRange(Base.planetLoader.planets.Values);
 
-            if (planets2 != planets) Create();
+            if (planets2 != planets) CreateDefault();
             
             OpenMenu(CancelButton.Cancel, SFS.Input.CloseMode.Current, menuElements.ToArray());
         }
---
+++ Warpinator/src/SearchHandler.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Media;
+using SFS.Input;
+using SFS.Translations;
+using SFS.UI;
+using SFS.UI.ModGUI;
+using SFS.WorldBase;
+using UnityEngine;
+using SoundPlayer = SFS.Audio.SoundPlayer;
+using Type = SFS.UI.ModGUI.Type;
+
+namespace Warpinator
+{
+    public static class SearchHandler
+    {
+        const float windowScale = 0.75f;
+        public static void OpenMenu()
+        {
+            Menu.textInput.Open("Back", "Search", OnSearch, CloseMode.Current, TextInputMenu.Element("Planet Search", string.Empty));
+
+            void OnSearch(string[] value)
+            {
+                var searchResults = FindMatches(value[0]);
+                
+                switch (searchResults.Count)
+                {
+                    case 0:
+                        MenuGenerator.ShowChoices(() => "No results found.", ButtonBuilder.CreateButton(null, () => "Okay", null, CloseMode.Current));
+                        break;
+                    case 1:
+                        PlanetTeleportMenu.Open(searchResults[0]);
+                        break;
+                    default:
+                        OpenResultsMenu(searchResults);
+                        break;
+                }
+            }
+        }
+
+        static List<Planet> FindMatches(string input)
+        {
+            input = input.ToLowerInvariant();
+            List<Planet> matches = new();
+            if (input == string.Empty) return matches;
+
+            foreach (Planet planet in PlanetSelectMenu.planets)
+            {
+                if (planet.name.ToLowerInvariant().Contains(input))
+                {
+                    matches.Add(planet);
+                }
+            }
+            return matches;
+        }
+
+        static void OpenResultsMenu(List<Planet> planets)
+        {
+            int columns = Mathf.Clamp((int)Math.Ceiling((double)(planets.Count / 13)), 2, 4);
+            int rows = Mathf.Clamp((int)Math.Ceiling((double)planets.Count / columns), 2, 13);
+            
+            var menuElement = new MenuElement(delegate(GameObject root)
+            {
+                var containerObject = new GameObject("ModGUI Container");
+                var rectTransform = containerObject.AddComponent<RectTransform>();
+                rectTransform.sizeDelta = new Vector2(0, 0);
+                
+                var scroll = Builder.CreateWindow(rectTransform, Builder.GetRandomID(), 275 * columns, 50 + 58 * rows, 0, 0, false, false, 1, "Search Results");
+
+                scroll.Position = new Vector2(0, scroll.Size.y * windowScale / 2);
+                
+                var layout = scroll.CreateLayoutGroup(Type.Vertical);
+                layout.spacing = 7;
+                layout.childAlignment = TextAnchor.MiddleCenter;
+                scroll.EnableScrolling(Type.Vertical);
+                if (planets.Count <= 2) Builder.CreateSpace(scroll, 0, 25);
+                PlanetSelectMenu.CreatePlanetButtons(planets, scroll);
+                
+                scroll.gameObject.transform.localScale = new Vector3(windowScale, windowScale);
+                containerObject.transform.SetParent(root.transform);
+            });
+
+            MenuElement[] elements = { menuElement };
+            MenuGenerator.OpenMenu(CancelButton.Cancel, CloseMode.Current, elements);
+        }
+    }
+}
